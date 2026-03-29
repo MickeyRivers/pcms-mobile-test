c25ae2789e157ee430b54396c4636bd89af0b9e5
MickeyRivers <mick@flow-cert.com>
2026-03-29 10:21:13 -0500

    Point to right DB

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PCMS Service Worker v70
-const CACHE_NAME = 'pcms-v79';
+const CACHE_NAME = 'pcms-v82';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(self.skipWaiting());
