c4948497d950d047290962790a57baaf4eca4162
MickeyRivers <mick@flow-cert.com>
2026-04-03 01:01:48 -0500

    fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PCMS Service Worker v70
-const CACHE_NAME = 'pcms-v104';
+const CACHE_NAME = 'pcms-v105';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(self.skipWaiting());
